412b34fb5f1717f7796b8d8704ec4932531d027e
belamrani <benoit.elamrani@gmail.com>
2018-06-29 22:55:32 +0200

    first commit

---
+++ src/tooling-git/index.ts
@@ -0,0 +1,85 @@
+import {
+    apply, chain, mergeWith, move, Rule, SchematicContext, SchematicsException, template, Tree,
+    url
+} from '@angular-devkit/schematics';
+import {NodePackageInstallTask} from "@angular-devkit/schematics/tasks";
+
+export function ToolingGitSchematic(): Rule {
+    const copyConfigFiles = apply(url('./files'), [
+        template({}),
+        move('./')
+    ]);
+
+    return chain([
+        () => {
+            // Show the options for this Schematics.
+            console.log('🐬️  Customize your environment with some cool stuffs');
+        },
+        mergeWith(copyConfigFiles),
+        addLibsToPackageJson(),
+        addPackageJsonConfig()
+    ]);
+}
+
+/** Add needed lib to package.json if not already present. */
+function addLibsToPackageJson() {
+    return (host: Tree, context: SchematicContext) => {
+        addPackageToPackageJson(host, 'dependencies', 'commitizen', '2.10.1');
+        addPackageToPackageJson(host, 'dependencies', 'cz-customizable', '5.2.0');
+        addPackageToPackageJson(host, 'dependencies', 'husky', '0.14.3');
+        addPackageToPackageJson(host, 'dependencies', 'lint-staged', '7.2.0');
+        addPackageToPackageJson(host, 'dependencies', 'prettier', '1.13.7');
+        context.addTask(new NodePackageInstallTask());
+        return host;
+    };
+}
+
+const addPackageJsonConfig = (): Rule => {
+    return (host: Tree) => {
+        const packageJsonFile = 'package.json';
+        const buffer = host.read(packageJsonFile);
+        if (buffer === null) {
+            throw new SchematicsException(`Could not read file: ${packageJsonFile}`);
+        }
+        const packageJson = JSON.parse(buffer.toString());
+        if (!packageJson.scripts['precommit']) {
+            packageJson.scripts['precommit'] = 'lint-staged';
+        }
+        if (!packageJson.scripts['commit']) {
+            packageJson.scripts['commit'] = 'git-cz';
+        }
+        if (!(packageJson['config'] && packageJson['config'].commitizen)) {
+            packageJson['config'] = {
+                "commitizen": {
+                    "path": "node_modules/cz-customizable"
+                },
+                "cz-customizable": {
+                    "config": ".cz-config.js"
+                }
+            };
+        }
+
+        host.overwrite(packageJsonFile, JSON.stringify(packageJson, null, 2));
+        return host;
+    };
+};
+
+
+/**
+ * Adds a package to the package.json
+ */
+function addPackageToPackageJson(
+    host: Tree, type: string, pkg: string, version: string): Tree {
+    if (host.exists('package.json')) {
+        const sourceText = host.read('package.json')!.toString('utf-8');
+        const json = JSON.parse(sourceText);
+        if (!json[type]) {
+            json[type] = {};
+        }
+        if (!json[type][pkg]) {
+            json[type][pkg] = version;
+        }
+        host.overwrite('package.json', JSON.stringify(json, null, 2));
+    }
+    return host;
+}
